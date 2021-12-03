d7e43469cf1f77508644031ca45ee509aaaa5d97
Garrett Hurst <pidgions21@gmail.com>
2021-12-03 10:24:58 -0500

    finishe D1C1 -g added welcome texts

---
+++ garrett/d1p1-depthIncrease.py
@@ -0,0 +1,17 @@
+
+#gets the data from the text file
+with open("garrett\day1-input.txt", "r") as f:
+    data = f.readlines()
+
+#cleans data
+for x in range(len(data)):
+    data[x] = int(data[x])
+    
+#checks for increases
+increases = 0
+for x in range(len(data) - 1):
+    if data[x] < data[x + 1]:
+        increases += 1
+    #print("%d   %d" % (data[x], increases))
+
+print(increases)
