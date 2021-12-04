a40361dc1ed6b07e738aa0b10764e3d00b40ca16
garrett h <hurstg02@gmail.com>
2021-12-04 00:14:12 -0500

    started d3p1

--- garrett/day3/d3p1-oxygen.py
+++ garrett/day3/d3p1-oxygen.py
@@ -1,25 +1,31 @@
-  
+import copy
+
 f = open("garrett\day3\d3p1-input.txt", "r")
 
+data = []
 ones = [0] * 12 #empty list to store the amount of ones
 values = 0      #keeps track of how many elements have been counted
 
 #loops through the file until the end is reached
-stillData = True
-while stillData:
-    data = f.readline()
-    if data:
-        #gets the data and converts the binary to an integer
-        data = int(data, 2) 
-
+stillLines = True
+while stillLines:
+    read = f.readline()
+    if read:
+        #adds the read value to a list as a string
+        s = str(read).replace('\n', '')
+        data.append(s)
+        
+        #converts the binary to an integer
+        read = int(read, 2) 
+        
         #incraments each bit in a list
         for i in range(len(ones)):
-            ones[i] += bool(data & 2**(len(ones) - i - 1))
+            ones[i] += bool(read & 2**(len(ones) - i - 1))
         
         #incraments vow many values have been read
         values += 1
     else:
-        stillData = False
+        stillLines = False
 
 gamma = 0
 epsilon = 0
@@ -29,6 +35,9 @@ for i in range(len(ones)):
     gamma += ones[i] * 2**(len(ones) - i - 1)
     epsilon += (1 - ones[i]) * 2**(len(ones) - i - 1)
 
-print('number:     ', bin(gamma))
-print('compliment: ', bin(epsilon))
-print('multiplied value: ', gamma * epsilon)
+data.sort(reverse=True)
+print(data)
+
+oxygen = 0
+co2    = 0
+print('o2: %d  co2: %d\nmultiplied: %d' % (oxygen, co2, oxygen * co2))
