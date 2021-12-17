863af5e48e8f7995e79f275710d4d1ddc7e16f57
Garrett Hurst <pidgions21@gmail.com>
2021-12-16 23:04:41 -0500

    completed d9p2

--- garrett/day9/d9p2-basins.py
+++ garrett/day9/d9p2-basins.py
@@ -1,55 +1,30 @@
-topMap = [x.rstrip('\n') for x in open('garrett\day9\\testimput.txt', 'r')]
+topMap = [x.rstrip('\n') for x in open('garrett\day9\d9-input.txt', 'r')]
 
-#calculates and returns the size of the basin
+#calculates and returns the size of the basin plus one
 def getBasinSize(x, y, topMap, visitedPoints):
     size = 1
 
-    #checks if the value is an edge point
-    def edgeStop(direction):
-        val = False
-        if direction == 'w':
-            if x == 1: pass
-            elif topMap[y][x - 1] >= topMap[y][x - 2]: val = True
-        
-        if direction == 'e':
-            if x == len(topMap[y]) - 2 or x == len(topMap[y]) - 1: pass
-            elif topMap[y][x + 1] >= topMap[y][x + 2]: val = True
-        
-        if direction == 'n':
-            if y == 1: pass
-            elif topMap[y - 1][x] >= topMap[y - 2][x]: val = True
-        
-        if direction == 's':
-            if y == len(topMap) - 2 or y == len(topMap) - 1: pass
-            elif topMap[y + 1][x] >= topMap[y + 2][x]: val = True
-        
-        return val
-
     #checks west
-    if x == 0 or (x - 1, y) in visitedPoints or edgeStop('w'): pass
-    elif topMap[y][x] < topMap[y][x - 1]: 
-        #print('moving to ', x-1, y)
+    if x == 0 or (x - 1, y) in visitedPoints: pass
+    elif topMap[y][x - 1] < '9': 
         visitedPoints.append((x - 1, y))
         size += getBasinSize(x - 1, y, topMap, visitedPoints)
 
     #checks east
-    if x == len(topMap[y]) - 1 or (x + 1, y) in visitedPoints or edgeStop('e'): pass
-    elif topMap[y][x] < topMap[y][x + 1]: 
-        #print('moving to ', x+1, y)
+    if x == len(topMap[y]) - 1 or (x + 1, y) in visitedPoints: pass
+    elif topMap[y][x + 1] < '9': 
         visitedPoints.append((x + 1, y))
         size += getBasinSize(x + 1, y, topMap, visitedPoints)
 
     #checks north
-    if y == 0 or (x, y - 1) in visitedPoints or edgeStop('n'): pass
-    elif topMap[y][x] < topMap[y - 1][x]: 
-        #print('moving to ', x, y-1)
+    if y == 0 or (x, y - 1) in visitedPoints: pass
+    elif topMap[y - 1][x] < '9': 
         visitedPoints.append((x, y - 1))
         size += getBasinSize(x, y - 1, topMap, visitedPoints)
         
     #checks south
-    if y == len(topMap) - 1 or (x, y + 1) in visitedPoints or edgeStop('s'): pass
-    elif topMap[y][x] < topMap[y + 1][x]: 
-        #print('moving to ', x, y+1)
+    if y == len(topMap) - 1 or (x, y + 1) in visitedPoints: pass
+    elif topMap[y + 1][x] < '9': 
         visitedPoints.append((x, y + 1))
         size += getBasinSize(x, y + 1, topMap, visitedPoints)
 
@@ -78,10 +53,16 @@ for y in range(len(topMap)):
         #incrament
         if lowCount == 4: lowPoints.append((x, y))
 
-
-print(lowPoints)
-biggest = 0
+#gets the sizes of all the basins in the map
+sizes = []
 for point in lowPoints:
-    biggest = getBasinSize(point[0], point[1], topMap, [])
-    print(biggest)
+    sizes.append(getBasinSize(point[0], point[1], topMap, []) - 1)
+
+#sums the largest three elements
+sum = 1
+for i in range(3):
+    index = sizes.index(max(sizes))
+    sum *= sizes.pop(index)
+
+print(f'the sum is {sum}')
 
