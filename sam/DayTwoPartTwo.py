98a4905b42687036d061df8bbbdc510b6d9ee20f
shorowitz03 <62364765+shorowitz03@users.noreply.github.com>
2021-12-18 15:36:59 -0500

    Add files via upload

---
+++ sam/DayTwoPartTwo.py
@@ -0,0 +1,23 @@
+path = './AdventOfCodeDay2InputFile.txt'
+fileID = open(path, 'r')
+fileClean = [line.split(" ") for line in fileID.readlines()]
+horizontal = 0
+vertical = 0
+aim = 0
+
+for lines in fileClean:
+    if(lines[0] == 'forward'):
+        horizontal += int(lines[1])
+        vertical += aim * int(lines[1])
+    elif(lines[0] == 'up'):
+        aim -= int(lines[1])
+    elif(lines[0] == 'down'):
+        aim += int(lines[1])
+
+
+product = horizontal * vertical
+#print(fileClean)
+
+print("Product: " + str(product))
+print("Vertical: " + str(vertical))
+print("Horizontal: " + str(horizontal))
